41073943e6182997e1157163a919b0d49220fbc8
vivek0913 <83529785+vivek0913@users.noreply.github.com>
2022-11-24 20:57:32 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,23 +5,15 @@ function About() {
 		<div id='About'>
 			<h1 style={{ textAlign: 'center' }}>About The Application</h1>
 			<div className='row'>
-				<div className='col-md-6'>
-					<p>
-						This is an open source app which is built for showing some programming skills purpose, for sure
-						i could`ve created a system that enables any username and password to login but there`s no need
-						for this
-					</p>
-					<p>
-						This app has no authentication (signup/login) system which means that any one can start using,
-						adding into, removing from, and editing to this system{' '}
-					</p>
-				</div>
+			
 				<div className='col-md-6'>
 					<p>
 						This is a school management system in which you can manage the school subjects, teachers,
-						students and classes
+						students and classes.
+						
+						We can add Student's details, Subject details, Teacher's details and Class details as well.
 					</p>
-					<p>Start using the app, navigate from the navbar at the right side, happy hacking :)</p>
+
 				</div>
 			</div>
 		</div>
